670e1b81d028fa5ffbc5470b7e3510ee6bd5cb19
Maria Luisa <marialuisafmendonca0@gmail.com>
2026-02-16 22:20:12 -0300

    Explicitando que não pode ser negativo

--- src/modulo01_basico/capitulo01/FatorialComFor.java
+++ src/modulo01_basico/capitulo01/FatorialComFor.java
@@ -4,17 +4,25 @@ import java.util.Scanner;
 public class FatorialComFor {
     public static void main(String[] args){
 
-        System.out.println("Digite um número para acharmos o seu fatorial");
+        System.out.println("Digite um número não-negativo para acharmos o seu fatorial");
         Scanner leitor = new Scanner(System.in);
         int numero = leitor.nextInt();
         int fatorial = numero;
+        int flag = 1;
 
-        for (int i = numero - 1; i > 1 ; i--){
-            fatorial *= i;
+        if(numero > 0) {
+            for (int i = numero - 1; i > 1; i--) {
+                fatorial *= i;
+                flag = 0;
+            }
+        } else {
+            System.out.print("Inválido, é um número não-negativo ");
         }
 
-        System.out.print("O fatorial de " + numero + " é " + fatorial);
-        leitor.close();
+        if(flag == 0) {
+            System.out.print("O fatorial de " + numero + " é " + fatorial);
+            leitor.close();
+        }
 
     }
 }
